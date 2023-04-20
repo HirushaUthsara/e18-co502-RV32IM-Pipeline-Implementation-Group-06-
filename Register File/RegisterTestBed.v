2561e52b61bb3d273962ac9e196f14ead957ae51
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-04-20 11:04:12 +0530

    final testing

--- Register File/RegisterTestBed.v
+++ Register File/RegisterTestBed.v
@@ -1,56 +1,62 @@
-`timescale 1ns / 100ps
 `include "RegisterFile.v"
+`timescale 1ns/1ps
 
-module testbench();
+module register_file_tb;
 
-reg [4:0] reg_write;
-reg [31:0] write_data;
-reg [4:0] read1;
-reg [4:0] read2;
-wire [31:0] read_data1;
-wire [31:0] read_data2;
-reg clk;
-reg rst;
-
-register_file myreg(
+  // Declare the DUT (device under test)
+  reg [4:0] reg_write;
+  reg [31:0] write_data;
+  reg [4:0] read1, read2;
+  wire [31:0] read_data1, read_data2;
+  reg writeenable;
+  reg clk, rst;
+  
+  register_file dut (
     .reg_write(reg_write),
     .write_data(write_data),
     .read1(read1),
     .read2(read2),
     .read_data1(read_data1),
     .read_data2(read_data2),
+    .writeenable(writeenable),
     .clk(clk),
     .rst(rst)
-);
-
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-end
-
-initial begin
+  );
+  
+  // Toggle the clock signal
+  always #5 clk = ~clk;
+  
+  // Initialize testbench inputs
+  initial begin
+    // Reset the DUT
     rst = 1;
     #10 rst = 0;
-end
-
-initial begin
-    // Test write operation
-    reg_write = 1;
-    write_data = 32'habcdef01;
-    #10;
     
-    // Test read operation
-    read1 = 1;
-    read2 = 0;
-    #10;
-    
-    // Verify read data
-    if (read_data1 != write_data || read_data2 != 0) begin
-        $error("Read data mismatch");
+    // Random write and read operations
+    repeat(1000) begin
+      // Random write operation
+      if ($random % 2 == 0) begin
+        reg_write = $random % 32;
+        write_data = $random;
+        writeenable = 1;
+        #10;
+      end
+      
+      // Random read operation
+      read1 = $random % 32;
+      read2 = $random % 32;
+      #10;
     end
     
-    $display("Test complete");
-    $finish;
-end
-
+    // End simulation
+    #10 $finish;
+  end
+  
+  // Dump waveform into a VCD file
+  initial begin
+    $dumpfile("register_file_waveform.vcd");
+    $dumpvars(0, register_file_tb);
+    #10;
+  end
+  
 endmodule
